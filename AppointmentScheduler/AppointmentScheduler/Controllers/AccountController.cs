6d0b256c0ebd31d43d4aefaf9114cd303265e44a
krithika117 <krithikanithyanandam1@gmail.com>
2023-04-16 11:57:04 +0530

    Register, Login, Logoff implemented

--- AppointmentScheduler/AppointmentScheduler/Controllers/AccountController.cs
+++ AppointmentScheduler/AppointmentScheduler/Controllers/AccountController.cs
@@ -26,6 +26,21 @@ namespace AppointmentScheduler.Controllers
 		{
 			return View();
 		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Login(LoginViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+				if (result.Succeeded)
+				{
+					return RedirectToAction("Index", "Home");
+				}
+				ModelState.AddModelError("", "Invalid login attempt");
+			}
+			return View(model);
+		}
         public async Task<IActionResult> Register()
 		{
 			if (!_roleManager.RoleExistsAsync(Helper.Admin).GetAwaiter().GetResult()) {
@@ -48,15 +63,26 @@ namespace AppointmentScheduler.Controllers
 					Name = model.Name
 				};
 
-				var result = await _userManager.CreateAsync(user);
+				var result = await _userManager.CreateAsync(user, model.Password);
 				if (result.Succeeded) {
 					await _userManager.AddToRoleAsync(user, model.RoleName);
 					await _signInManager.SignInAsync(user, isPersistent: false);
 					return RedirectToAction("Index", "Home");
 				}
+				foreach(var error in result.Errors)
+				{
+					ModelState.AddModelError("",error.Description);
+				}
 			}
-			return View();
+			return View(model);
 
 		}
+		[HttpPost]
+		public async Task<IActionResult> LogOff()
+		{
+			await _signInManager.SignOutAsync();
+			return RedirectToAction("Login","Account");
+		}
 	}
+
 }
