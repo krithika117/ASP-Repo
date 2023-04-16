d4921be249967f8f4e343c73f11079a8a7ebbcfe
krithika117 <krithikanithyanandam1@gmail.com>
2023-04-16 11:02:26 +0530

    initial commit for appointment scheduler

---
+++ AppointmentScheduler/AppointmentScheduler/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using AppointmentScheduler.Models;
+using AppointmentScheduler.Models.ViewModels;
+using AppointmentScheduler.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentScheduler.Controllers
+{
+	public class AccountController : Controller
+	{
+
+		private readonly ApplicationDbContext _db;
+		UserManager<ApplicationUser> _userManager;
+		SignInManager<ApplicationUser> _signInManager;
+		RoleManager<IdentityRole> _roleManager;
+        public AccountController(ApplicationDbContext db, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+			_db = db;
+			_signInManager = signInManager;
+			_userManager = userManager;
+			_roleManager = roleManager;
+
+        }
+
+        public IActionResult Login()
+		{
+			return View();
+		}
+        public async Task<IActionResult> Register()
+		{
+			if (!_roleManager.RoleExistsAsync(Helper.Admin).GetAwaiter().GetResult()) {
+				await _roleManager.CreateAsync(new IdentityRole(Helper.Admin));
+				await _roleManager.CreateAsync(new IdentityRole(Helper.Manager));
+				await _roleManager.CreateAsync(new IdentityRole(Helper.Associate));
+			}
+			return View();
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Register(RegisterViewModel model)
+		{
+			// Server side validation
+			if (ModelState.IsValid) {
+				var user = new ApplicationUser
+				{
+					UserName = model.Email,
+					Email = model.Email,
+					Name = model.Name
+				};
+
+				var result = await _userManager.CreateAsync(user);
+				if (result.Succeeded) {
+					await _userManager.AddToRoleAsync(user, model.RoleName);
+					await _signInManager.SignInAsync(user, isPersistent: false);
+					return RedirectToAction("Index", "Home");
+				}
+			}
+			return View();
+
+		}
+	}
+}
---
+++ AppointmentScheduler/AppointmentScheduler/Utility/Helper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AppointmentScheduler.Utility
+{
+	public class Helper
+	{
+		public static string Admin = "Admin";
+		public static string Manager = "Manager";
+		public static string Associate = "Associate";
+
+        public static List<SelectListItem> GetRolesForDropDown(bool isAdmin)
+        {
+            if (isAdmin)
+            {
+                return new List<SelectListItem>
+                {
+                    new SelectListItem{Value=Helper.Admin,Text=Helper.Admin}
+                };
+            }
+            else
+            {
+                return new List<SelectListItem>
+                {
+                    new SelectListItem{Value=Helper.Manager,Text=Helper.Manager},
+                    new SelectListItem{Value=Helper.Associate,Text=Helper.Associate}
+                };
+            }
+        }
+    }
+}
