fdafc3aa99e2d3831c0a943074480d2347020dab
krithika117 <krithikanithyanandam1@gmail.com>
2023-04-19 19:44:01 +0530

    Changes in view

--- Text-Editor-App/AuthenticationTextEditor/Program.cs
+++ Text-Editor-App/AuthenticationTextEditor/Program.cs
@@ -1,11 +1,8 @@
+using AuthenticationTextEditor.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using Newtonsoft.Json.Linq;
-using System.Configuration;
-using Text_Editor_App.Models;
 
-namespace Text_Editor_App
+namespace AuthenticationTextEditor
 {
     public class Program
     {
@@ -14,16 +11,23 @@ namespace Text_Editor_App
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(connectionString));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -34,15 +38,15 @@ namespace Text_Editor_App
             app.UseStaticFiles();
 
             app.UseRouting();
-                        app.UseAuthentication();;
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
 
-            //app.MapRazorPages();
             app.Run();
         }
     }
